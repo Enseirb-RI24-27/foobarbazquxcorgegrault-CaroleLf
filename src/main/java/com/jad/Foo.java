c95615dd1d99f49453c64ab871e3f097ed12e180
Carole <caleflohic@enseirb-matmeca.fr>
2026-01-12 17:50:54 +0100

    Commit 12

--- src/main/java/com/jad/Foo.java
+++ src/main/java/com/jad/Foo.java
@@ -1,63 +1,58 @@
 package com.jad;
 
 public class Foo {
+    private Bar bar;
+    private Baz[] bazs;
+    private Qux qux;
+    private Corge corge;
+    private Grault[] graults;
+
+    public Foo(Bar bar) {
+        this.bar = bar;
+        this.bazs = new Baz[0];
+        this.graults = new Grault[0];
+    }
+
+    public void addBaz(Baz baz) {
+        Baz[] newBazs = new Baz[this.bazs.length + 1];
+        System.arraycopy(this.bazs, 0, newBazs, 0, this.bazs.length);
+        newBazs[this.bazs.length] = baz;
+        this.bazs = newBazs;
+    }
+
+    public void addGrault() {
+        Grault newGrault = new Grault(this);
+        Grault[] newGraults = new Grault[this.graults.length + 1];
+        System.arraycopy(this.graults, 0, newGraults, 0, this.graults.length);
+        newGraults[this.graults.length] = newGrault;
+        this.graults = newGraults;
+    }
 
     public Bar getBar() {
         return this.bar;
     }
 
-
     public Baz[] getBazs() {
         return this.bazs;
     }
-
 
     public Qux getQux() {
         return this.qux;
     }
 
-
+    public void setQux(Qux qux) {
+        this.qux = qux;
+    }
 
     public Corge getCorge() {
         return this.corge;
     }
 
-
-
-    public Grault[] getGraults() {
-        return this.graults;
-    }
-
-
-
-    private Bar bar;
-
-    private Baz[] bazs;
-
-    private  Qux qux;
-
-    private Corge corge;
-
-    private Grault[] graults;
-
     public void setCorge(Corge corge) {
         this.corge = corge;
     }
 
-    public Foo(Bar bar){
-        bar = new Bar();
-        this.bar = bar;
-        bazs = new Baz[0];
-        qux = new Qux();
-        corge = new Corge(this);
-        graults = new Grault[0];
-    }
-
-    public void addBaz(Baz baz){
-        bazs[bazs.length] = baz;
-    }
-
-    public void addGrault(){
-        graults[graults.length] = new Grault(this);
+    public Grault[] getGraults() {
+        return this.graults;
     }
 }
