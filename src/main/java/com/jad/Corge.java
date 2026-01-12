b839be3aba9f479faeeb3f827a7cb48e895bb037
Carole <caleflohic@enseirb-matmeca.fr>
2026-01-12 17:48:35 +0100

    Commit 11

--- src/main/java/com/jad/Corge.java
+++ src/main/java/com/jad/Corge.java
@@ -3,17 +3,24 @@ package com.jad;
 public class Corge {
     private Foo foo;
 
+    public Corge(Foo foo) {
+        this.foo = foo;
+        if (foo != null) {
+            foo.setCorge(this);
+        }
+    }
+
     public Foo getFoo() {
         return this.foo;
     }
 
     public void setFoo(Foo foo) {
+        if (this.foo != null) {
+            this.foo.setCorge(null);
+        }
         this.foo = foo;
+        if (foo != null) {
+            foo.setCorge(this);
+        }
     }
-
-    public Corge(Foo foo) {
-        this.foo = foo;
-    }
-
-
 }
